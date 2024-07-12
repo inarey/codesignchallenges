e3e5a6795a4cac9f1194850f066dcc5aef6518a6
shan-ti <thoselovelywords@gmail.com>
2024-07-12 17:06:00 +0900

    Started a code design challenges

---
+++ src/assets/images.jsx
@@ -0,0 +1,11 @@
+import arrow from './arrow-down 4.png'
+import bell from './bell.png'
+import logo from './escodesign-logo.png'
+import moon from './moon.png'
+import search from './search.png'
+import share from './share.png'
+import sun from './sun.png'
+import thumb from './thumbsup.png'
+import watermark from './watermark.png'
+
+export {arrow, bell, logo, moon, search, share, sun, thumb, watermark}
